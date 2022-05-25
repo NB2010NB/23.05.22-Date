a6e2819451110b2f009b8549ff885537cd9ebbbc
NB2010NB <natanbalai1@gmail.com>
2022-05-25 22:23:39 +0300

    date

---
+++ main.js
@@ -0,0 +1,134 @@
+// ********* 163 ********** * /
+// function newdate() {
+//     var mydate = new Date()
+//     console.log(mydate)
+// } newdate()
+
+//     ****** 164 ******** * /
+// function printcons(date1) {
+//     date1 = new Date(date1)
+//     console.log(date1)
+// } printcons("10/20/96")
+
+//     ****** 165 **********
+//     function printcon() {
+//         var date2 = prompt("inter date");
+//         var date3 = new Date(date2);
+//         console.log(date3)
+//     }printcon()
+
+
+//         ********* 166 ******** * /
+// function returnmanth() {
+//     var datesers = prompt("inter date");
+//     var mtj = new Date(datesers)
+//     console.log(mtj.getMonth());
+// } returnmanth()
+
+//     ******** 167 ******** * /
+// function returnHour() {
+//     var dateusers = prompt("inter date");
+//     var hours = new Date(dateusers);
+//     console.log(hours.getHours());
+// } returnHour()
+
+//     ***** 168 ******** * /
+// function returnYear() {
+//     var dateuser = prompt("inter date");
+//     var years = new Date(dateuser);
+//     console.log(years.getFullYear())
+// } returnYear()
+
+//     ****** 169 ****** * /
+// function returnMinit() {
+//     var dateUser = prompt("inter date");
+//     var minute = new Date(dateUser);
+//     console.log(minute.getMinutes());
+// } returnMinit()
+
+//     ********** 170 ********** * /
+// function bigDate() {
+//     var date1 = prompt("inter date");
+//     var data1 = new Date(date1);
+//     var det1 = data1.getFullYear();
+//     var date2 = prompt("inter date");
+//     var data2 = new Date(date2.getFullYear());
+//     var det2 = data2.getFullYear()
+//     document.write(Math.min(data1, data2));
+//     console.log(Math.min(data1, data2));
+// } bigDate()
+
+//     ******* 171 ******** * /
+
+// function somefunction() {
+//     var dateusers = prompt("inter datte")
+//     var date = new Date(dateusers);
+//     var useryear = date.getFullYear();
+//     var nowdate = new Date();
+//     var nowyear = nowdate.getFullYear();
+//     var age = nowyear - useryear;
+//     console.log(age)
+// } somefunction()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var array = [3, 4, 22, 3, 2, 15, 7, 120]
+// function printNumber(array) {
+//     var maxNumber = 0;
+//     var minNumber = 1000;
+//     for (var i = 0; i < array.length; i++) {
+//         if (array[i] > maxNumber) {
+//             maxNumber = array[i]
+//         }
+//         if (array[i] < minNumber) {
+//             minNumber = array[i]
+//         }
+//     }
+//     console.log(maxNumber)
+//     var sum = minNumber * maxNumber
+//     return sum
+// } console.log(printNumber(array))
+
+
+// function number(array) {
+//     var maxnum = 0;
+//     var index = 0;
+//     for (var i = 0; i < array.length; i++) {
+//         if (array[i] > maxnum) {
+//             if (array[i] % 2 == 0) {
+//                 maxnum = array[i]
+//                 index = i
+//             }
+//         }
+//     }
+//     console.log(maxnum, index)
+
+// } number(array)
+
+
+
+
+
+
+
+
